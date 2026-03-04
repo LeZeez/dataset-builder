142b05a1a553e8abe5ef424573b528b0ed5aa7f2
Zeez <zaidrabee666@gmail.com>
2026-03-04 03:34:01 +0200

    Disable debug mode in server run configuration

--- server.py
+++ server.py
@@ -1180,4 +1180,4 @@ if __name__ == '__main__':
     print('   Server running at http://localhost:5000')
     print('   Press Ctrl+C to stop')
     
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=False, host='127.0.0.1', port=5000)
